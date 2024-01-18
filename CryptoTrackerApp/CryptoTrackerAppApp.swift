af067f718dceaec355f0948d5ad6ec8c50710d70
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-01-18 15:41:00 +0100

    Initial Commit

---
+++ CryptoTrackerApp/CryptoTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerAppApp.swift
+//  CryptoTrackerApp
+//
+//  Created by Александр Карадяур on 18.01.24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
